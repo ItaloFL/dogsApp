dd4e0d115b5c6725cdc0d7aae2da47c0e56dab17
Italo Ferreira <grd788@gmail.com>
2022-04-07 22:37:55 -0300

    feat :sparkles: Header components and style

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,15 +1,27 @@
-import { Link, Navigate } from 'react-router-dom'
+import { useContext } from 'react'
+import { Link } from 'react-router-dom'
 import Dogs from '../../assets/dogs.svg'
+import { UserContext } from '../../context/UserContext'
 import { HeaderWrapper, NavigateWrapper } from './style'
 
 export const Header = () => {
+  const { data, userLogout } = useContext(UserContext)
   return (
     <HeaderWrapper>
       <NavigateWrapper className="container">
         <Link to="/" aria-label="Dogs - Home">
-          <img className='logo' src={Dogs} alt="Icone Dogs" />
+          <img className="logo" src={Dogs} alt="Icone Dogs" />
         </Link>
-        <Link className='login' to="/login">Login / Criar</Link>
+        {data ? (
+          <Link className="login" to="/conta">
+            {data.name}
+            <button onClick={userLogout}>Sair</button>
+          </Link>
+        ) : (
+          <Link className="login" to="/login">
+            Login / Criar
+          </Link>
+        )}
       </NavigateWrapper>
     </HeaderWrapper>
   )
