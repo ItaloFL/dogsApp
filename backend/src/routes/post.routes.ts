5559b2a2f87f5b068ed34b044f22d22fc18b402a
Italo Ferreira <grd788@gmail.com>
2022-04-12 23:34:23 -0300

    feat :sparkles: alter routes and list and get routes

--- backend/src/routes/post.routes.ts
+++ backend/src/routes/post.routes.ts
@@ -3,12 +3,18 @@ import multer from 'multer'
 import { CreatePostController } from '../modules/post/useCases/createPost/CreatePostController'
 import multerConfig from '../config/multer'
 import { ensureAuthenticateUser } from '../middleware/ensureAuthenticateUser'
+import { ListPostController } from '../modules/post/useCases/listPost/ListPostController'
+import { GetPhotoPostController } from '../modules/post/useCases/getPhotoPost/GetPhotoPostController'
 
 const postRoutes = Router()
 
 const createPostController = new CreatePostController()
+const listPostController = new ListPostController()
+const getPhotoPostController = new GetPhotoPostController()
 const storage = multer(multerConfig)
 
-postRoutes.post('/createPost', storage.single('image'), createPostController.handle)
+postRoutes.post('/createPost', storage.single('image'), ensureAuthenticateUser, createPostController.handle)
+postRoutes.get('/post', listPostController.handle)
+postRoutes.get('/photo/:id', getPhotoPostController.handle)
 
 export { postRoutes }
