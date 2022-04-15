79bc29fe246728c8f2490b8bd5c12ac49ef04164
Italo Ferreira <grd788@gmail.com>
2022-04-14 23:20:29 -0300

    feat :sparkles: post coment route

--- backend/src/routes/post.routes.ts
+++ backend/src/routes/post.routes.ts
@@ -5,16 +5,19 @@ import multerConfig from '../config/multer'
 import { ensureAuthenticateUser } from '../middleware/ensureAuthenticateUser'
 import { ListPostController } from '../modules/post/useCases/listPost/ListPostController'
 import { GetPhotoPostController } from '../modules/post/useCases/getPhotoPost/GetPhotoPostController'
+import { CreateComentPostController } from '../modules/post/useCases/createComentPost/CreateComentPostController'
 
 const postRoutes = Router()
 
 const createPostController = new CreatePostController()
 const listPostController = new ListPostController()
 const getPhotoPostController = new GetPhotoPostController()
+const createComentPostController = new CreateComentPostController()
 const storage = multer(multerConfig)
 
-postRoutes.post('/createPost', storage.single('image'), ensureAuthenticateUser, createPostController.handle)
+postRoutes.post('/createPost/:id', ensureAuthenticateUser, storage.single('image'),  createPostController.handle)
 postRoutes.get('/post', listPostController.handle)
 postRoutes.get('/photo/:id', getPhotoPostController.handle)
+postRoutes.post('/coment/:author', createComentPostController.handle)
 
 export { postRoutes }
