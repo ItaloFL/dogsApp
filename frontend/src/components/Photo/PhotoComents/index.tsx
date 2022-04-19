fc8bb86ed4659c0416def5ef3170fd07f37aadb5
Italo Ferreira <grd788@gmail.com>
2022-04-18 23:00:01 -0300

    feat :sparkles: Photo components, fiels and styles

---
+++ frontend/src/components/Photo/PhotoComents/index.tsx
@@ -0,0 +1,8 @@
+
+
+
+export const PhotoComents = () => {
+  return (
+    <div></div>
+  )
+}
--- frontend/src/components/Photo/PhotoContent/index.tsx
+++ frontend/src/components/Photo/PhotoContent/index.tsx
@@ -1,17 +1,45 @@
+import { Link } from 'react-router-dom'
+import { PhotoComents } from '../PhotoComents'
+import { Atributes, Author, ImagePhoto, PhotoContainer, PhotoDatails } from './style'
 
+export type DataType = {
+  data: {
+    Coments: []
+    id: string
+    idade: string
+    image: string
+    name: string
+    peso: string
+    user: {
+      id: string
+      name: string
+      email: string
+    }
+  }
+}
 
-
-export const PhotoContent = ({ data }: any) => {
-
-  
+export const PhotoContent = ({ data }: DataType) => {
   return (
-    <div>
-      <div><img src={data.image} alt={data.name} /></div>
-      <div>
+    <PhotoContainer>
+      <ImagePhoto>
+        <img src={data.image} />
+      </ImagePhoto>
+      <PhotoDatails>
         <div>
-          <p></p>
+          <Author>
+            <Link to={`/profile/${data.user.name}`}>@{data.user.name}</Link>
+            <span>{data.peso}</span>
+          </Author>
+          <h1 className='title'>
+            <Link to={`/photo/${data.name}`}>@{data.name}</Link>
+          </h1>
+          <Atributes>
+             <li>{data.peso} kg</li>
+             <li>{data.idade} anos</li>
+          </Atributes>
         </div>
-      </div>
-    </div>
+      </PhotoDatails>
+      <PhotoComents id={data.id} comments={data.Coments}/>
+    </PhotoContainer>
   )
 }
