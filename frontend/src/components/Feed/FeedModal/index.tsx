66a64ea7579a30e6f4372350b38f35587b0141e6
Italo Ferreira <grd788@gmail.com>
2022-04-18 22:59:22 -0300

    feat :sparkles: feedModal styles and structure

--- frontend/src/components/Feed/FeedModal/index.tsx
+++ frontend/src/components/Feed/FeedModal/index.tsx
@@ -1,28 +1,28 @@
-import { useEffect } from "react";
-import { useFetch } from "../../../hooks/useFetch"
-import { PHOTO_GET } from "../../../services/api";
-import { Error } from "../../Helper";
-import { Loading } from "../../Helper/Loading";
-import { PhotoContent } from "../../Photo/PhotoContent";
+import React, { MouseEvent, MouseEventHandler, useEffect } from 'react'
+import { useFetch } from '../../../hooks/useFetch'
+import { PHOTO_GET } from '../../../services/api'
+import { Error } from '../../Helper'
+import { Loading } from '../../Helper/Loading'
+import { PhotoContent } from '../../Photo/PhotoContent'
+import { ModalWrapper } from './style'
 
-
-
-
-export const FeedModal = ({photo}: any) => {
-
-  const { data, loading, error, request } = useFetch();
+export const FeedModal = ({ photo, setModalPhoto }: any) => {
+  const { data, loading, error, request } = useFetch()
 
   useEffect(() => {
-    
     const { url, options } = PHOTO_GET(photo.id)
     request(url, options)
   }, [photo, request])
 
+  function clickOutsideClick({ target, currentTarget }: MouseEvent) {
+    if(target === currentTarget) setModalPhoto(null)
+  }
+
   return (
-    <div>
-      {error && <Error error={error}/>}
+    <ModalWrapper onClick={clickOutsideClick}>
+      {error && <Error error={error} />}
       {loading && <Loading />}
-      {data && <PhotoContent data={data}/>}
-    </div>
+      {data && <PhotoContent data={data} />}
+    </ModalWrapper>
   )
 }
