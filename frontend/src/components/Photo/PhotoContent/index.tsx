b6b38da27c2612fe2ea2f136351c65bef352d8c2
Italo Ferreira <grd788@gmail.com>
2022-04-20 23:41:00 -0300

    feat :sparkles: content photo finish and style

--- frontend/src/components/Photo/PhotoContent/index.tsx
+++ frontend/src/components/Photo/PhotoContent/index.tsx
@@ -1,10 +1,19 @@
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { PhotoComents } from '../PhotoComents'
-import { Atributes, Author, ImagePhoto, PhotoContainer, PhotoDatails } from './style'
+import { UserContext, UserType } from '../../../context/UserContext'
+import { PhotoComents, PostType } from '../PhotoComents'
+import { PhotoDelete } from '../PhotoDelete'
+import {
+  Atributes,
+  Author,
+  ImagePhoto,
+  PhotoContainer,
+  PhotoDatails
+} from './style'
 
 export type DataType = {
   data: {
-    Coments: []
+    Coments: CommentType[]
     id: string
     idade: string
     image: string
@@ -18,7 +27,15 @@ export type DataType = {
   }
 }
 
+export type CommentType = {
+  id: string
+  user: UserType
+  postId: PostType
+  coment: string
+}
+
 export const PhotoContent = ({ data }: DataType) => {
+  const user = useContext(UserContext)
   return (
     <PhotoContainer>
       <ImagePhoto>
@@ -27,19 +44,23 @@ export const PhotoContent = ({ data }: DataType) => {
       <PhotoDatails>
         <div>
           <Author>
-            <Link to={`/profile/${data.user.name}`}>@{data.user.name}</Link>
+            {user.data && user.data.name === data.user.name ? (
+              <PhotoDelete id={data.id} />
+            ) : (
+              <Link to={`/profile/${data.user.name}`}>@{data.user.name}</Link>
+            )}
             <span>{data.peso}</span>
           </Author>
-          <h1 className='title'>
+          <h1 className="title">
             <Link to={`/photo/${data.name}`}>@{data.name}</Link>
           </h1>
           <Atributes>
-             <li>{data.peso} kg</li>
-             <li>{data.idade} anos</li>
+            <li>{data.peso} kg</li>
+            <li>{data.idade} anos</li>
           </Atributes>
         </div>
       </PhotoDatails>
-      <PhotoComents id={data.id} comments={data.Coments}/>
+      <PhotoComents id={data.id} Coments={data.Coments} />
     </PhotoContainer>
   )
 }
