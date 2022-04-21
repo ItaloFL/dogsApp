904413501ee9b986ff05a98e57b87ce0aac39d54
Italo Ferreira <grd788@gmail.com>
2022-04-20 23:37:27 -0300

    feat :sparkles: delete Photo route

--- backend/src/routes/post.routes.ts
+++ backend/src/routes/post.routes.ts
@@ -6,6 +6,7 @@ import { ensureAuthenticateUser } from '../middleware/ensureAuthenticateUser'
 import { ListPostController } from '../modules/post/useCases/listPost/ListPostController'
 import { GetPhotoPostController } from '../modules/post/useCases/getPhotoPost/GetPhotoPostController'
 import { CreateComentPostController } from '../modules/post/useCases/createComentPost/CreateComentPostController'
+import { DeletePhotoController } from '../modules/post/useCases/deletePhoto/DeletePhotoController'
 
 const postRoutes = Router()
 
@@ -13,11 +14,13 @@ const createPostController = new CreatePostController()
 const listPostController = new ListPostController()
 const getPhotoPostController = new GetPhotoPostController()
 const createComentPostController = new CreateComentPostController()
+const deletePhotoController = new DeletePhotoController()
 const storage = multer(multerConfig)
 
 postRoutes.post('/createPost', ensureAuthenticateUser, storage.single('image'),  createPostController.handle)
 postRoutes.get('/post', listPostController.handle)
 postRoutes.get('/photo/:id', getPhotoPostController.handle)
-postRoutes.post('/coment', ensureAuthenticateUser, createComentPostController.handle)
+postRoutes.post('/coment/:postId', ensureAuthenticateUser, createComentPostController.handle)
+postRoutes.delete('/post/:id', ensureAuthenticateUser, deletePhotoController.handle)
 
 export { postRoutes }
